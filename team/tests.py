ce2e1b38bddccf6578d617459667481a2e3305c0
Tom Petit <thopet@uw.edu>
2015-07-05 13:46:07 -0700

    tests: team app: minor view status tests

--- team/tests.py
+++ team/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from __future__ import absolute_import
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+import testing.view_access as view_access
+import testing.control
+
+class TestProblemListView(TestCase):
+	def setUp(self):
+		testing.control.set_contest_in_session()
+
+	def test_view_exists(self):
+		team_user= testing.control.create_team()
+		status = view_access.get(view_name="problem list", user=team_user)
+		assert status == 200, "Expected 200 for view, got %d" % status
+
+class TestProblemDetailView(TestCase):
+	def setUp(self):
+		testing.control.set_contest_in_session()
+		self.problem = testing.control.create_problem(name="test problem", score=10)
+
+	def test_view_exists(self):
+		team_user= testing.control.create_team()
+		url = reverse("problem detail", args=[self.problem.id])
+		status = view_access.get(view_url=url, user=team_user)
+		assert status == 200, "Expected 200 for view, got %d" % status
+
+
+
